a7fca0bfe5857aab31622a77c7ca1113c9a1c939
Leonardo Anjos <las4@cin.ufpe.br>
2020-10-26 03:14:29 -0300

    Added logic for wrapping selected text in a span

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
 window.onmouseup = () => {
-    console.log(window.getSelection());
+    // TODO: Show box for editting text
+
+    const selection = window.getSelection();
+    for (let i = 0; i < selection.rangeCount; i++) {
+        const range = selection.getRangeAt(i);
+        const content = range.extractContents();
+
+        const span = document.createElement('span');
+        // TODO: Add style to text based on what the user asked for
+        span.appendChild(content);
+
+        range.insertNode(span);
+    }
 };
